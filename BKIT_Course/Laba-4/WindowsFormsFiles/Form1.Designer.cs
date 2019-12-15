5ca9bdd48fb6c10f95d340d3155f759a874af71b
Марат Камалов <dead_island_hd@bk.ru>
2019-12-15 19:02:08 +0300

    Laba-4

---
+++ BKIT_Course/Laba-4/WindowsFormsFiles/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFileReadTime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.buttonExact = new System.Windows.Forms.Button();
+            this.textBoxExactTime = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonLoadFile
+            // 
+            this.buttonLoadFile.Location = new System.Drawing.Point(21, 17);
+            this.buttonLoadFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLoadFile.Name = "buttonLoadFile";
+            this.buttonLoadFile.Size = new System.Drawing.Size(177, 35);
+            this.buttonLoadFile.TabIndex = 0;
+            this.buttonLoadFile.Text = "Чтение из файла";
+            this.buttonLoadFile.UseVisualStyleBackColor = true;
+            this.buttonLoadFile.Click += new System.EventHandler(this.buttonLoadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Время чтения из файла:";
+            // 
+            // textBoxFileReadTime
+            // 
+            this.textBoxFileReadTime.Location = new System.Drawing.Point(424, 22);
+            this.textBoxFileReadTime.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxFileReadTime.Name = "textBoxFileReadTime";
+            this.textBoxFileReadTime.ReadOnly = true;
+            this.textBoxFileReadTime.Size = new System.Drawing.Size(349, 26);
+            this.textBoxFileReadTime.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Слово для поиска:";
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(207, 115);
+            this.textBoxFind.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(296, 26);
+            this.textBoxFind.TabIndex = 4;
+            // 
+            // buttonExact
+            // 
+            this.buttonExact.Location = new System.Drawing.Point(22, 194);
+            this.buttonExact.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonExact.Name = "buttonExact";
+            this.buttonExact.Size = new System.Drawing.Size(176, 35);
+            this.buttonExact.TabIndex = 5;
+            this.buttonExact.Text = " Поиск";
+            this.buttonExact.UseVisualStyleBackColor = true;
+            this.buttonExact.Click += new System.EventHandler(this.buttonExact_Click);
+            // 
+            // textBoxExactTime
+            // 
+            this.textBoxExactTime.Location = new System.Drawing.Point(434, 198);
+            this.textBoxExactTime.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxExactTime.Name = "textBoxExactTime";
+            this.textBoxExactTime.ReadOnly = true;
+            this.textBoxExactTime.Size = new System.Drawing.Size(340, 26);
+            this.textBoxExactTime.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(252, 201);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Время поиска:";
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 20;
+            this.listBoxResult.Location = new System.Drawing.Point(23, 259);
+            this.listBoxResult.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(751, 404);
+            this.listBoxResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 914);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.textBoxExactTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonExact);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFileReadTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLoadFile);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Поиск в файле";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFileReadTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.Button buttonExact;
+        private System.Windows.Forms.TextBox textBoxExactTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxResult;
+    }
+}
+
